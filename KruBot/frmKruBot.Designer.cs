e72b742f105250869b9b519b9b3b23377c1fceec
Krutonium <PFCKrutonium@gmail.com>
2018-12-14 17:53:09 -0500

    Add project files.

---
+++ KruBot/frmKruBot.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace KruBot
+{
+    partial class frmKruBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tabChat = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.SidePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tbMusicVolume = new System.Windows.Forms.TrackBar();
+            this.tblInfo = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPlayerTime = new System.Windows.Forms.Label();
+            this.lblRequester = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tabYouTube = new System.Windows.Forms.TabPage();
+            this.SongStarter = new System.Windows.Forms.Timer(this.components);
+            this.TabControl.SuspendLayout();
+            this.tabChat.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SidePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMusicVolume)).BeginInit();
+            this.tblInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.tabChat);
+            this.TabControl.Controls.Add(this.tabYouTube);
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(817, 498);
+            this.TabControl.TabIndex = 0;
+            // 
+            // tabChat
+            // 
+            this.tabChat.Controls.Add(this.tableLayoutPanel1);
+            this.tabChat.Location = new System.Drawing.Point(4, 22);
+            this.tabChat.Name = "tabChat";
+            this.tabChat.Padding = new System.Windows.Forms.Padding(3);
+            this.tabChat.Size = new System.Drawing.Size(809, 472);
+            this.tabChat.TabIndex = 0;
+            this.tabChat.Text = "Chat";
+            this.tabChat.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.69975F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.30025F));
+            this.tableLayoutPanel1.Controls.Add(this.rtbChat, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbMsg, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.SidePanel, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.06009F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.939914F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(803, 466);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbChat.Location = new System.Drawing.Point(3, 3);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.Size = new System.Drawing.Size(601, 423);
+            this.rtbChat.TabIndex = 1;
+            this.rtbChat.Text = "";
+            this.rtbChat.TextChanged += new System.EventHandler(this.rtbChat_TextChanged);
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMsg.Location = new System.Drawing.Point(5, 439);
+            this.tbMsg.Margin = new System.Windows.Forms.Padding(5, 10, 5, 5);
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(597, 20);
+            this.tbMsg.TabIndex = 2;
+            this.tbMsg.TextChanged += new System.EventHandler(this.tbMsg_TextChanged_1);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.36842F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.63158F));
+            this.tableLayoutPanel2.Controls.Add(this.btnSendMessage, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnSkip, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(610, 432);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(190, 31);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(3, 3);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMessage.TabIndex = 3;
+            this.btnSendMessage.Text = "Send";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(92, 3);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(75, 23);
+            this.btnSkip.TabIndex = 4;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.ColumnCount = 2;
+            this.SidePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.42105F));
+            this.SidePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.57895F));
+            this.SidePanel.Controls.Add(this.tbMusicVolume, 0, 0);
+            this.SidePanel.Controls.Add(this.tblInfo, 1, 0);
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SidePanel.Location = new System.Drawing.Point(610, 3);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.RowCount = 1;
+            this.SidePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.64539F));
+            this.SidePanel.Size = new System.Drawing.Size(190, 423);
+            this.SidePanel.TabIndex = 5;
+            // 
+            // tbMusicVolume
+            // 
+            this.tbMusicVolume.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMusicVolume.Location = new System.Drawing.Point(3, 3);
+            this.tbMusicVolume.Maximum = 100;
+            this.tbMusicVolume.Name = "tbMusicVolume";
+            this.tbMusicVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tbMusicVolume.Size = new System.Drawing.Size(47, 417);
+            this.tbMusicVolume.TabIndex = 1;
+            this.tbMusicVolume.Value = 70;
+            // 
+            // tblInfo
+            // 
+            this.tblInfo.ColumnCount = 1;
+            this.tblInfo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblInfo.Controls.Add(this.lblPlayerTime, 0, 0);
+            this.tblInfo.Controls.Add(this.lblRequester, 0, 1);
+            this.tblInfo.Controls.Add(this.lblTitle, 0, 2);
+            this.tblInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblInfo.Location = new System.Drawing.Point(56, 3);
+            this.tblInfo.Name = "tblInfo";
+            this.tblInfo.RowCount = 4;
+            this.tblInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tblInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 339F));
+            this.tblInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tblInfo.Size = new System.Drawing.Size(131, 417);
+            this.tblInfo.TabIndex = 3;
+            // 
+            // lblPlayerTime
+            // 
+            this.lblPlayerTime.AutoSize = true;
+            this.lblPlayerTime.Location = new System.Drawing.Point(3, 0);
+            this.lblPlayerTime.Name = "lblPlayerTime";
+            this.lblPlayerTime.Size = new System.Drawing.Size(70, 13);
+            this.lblPlayerTime.TabIndex = 2;
+            this.lblPlayerTime.Text = "Current Time:";
+            // 
+            // lblRequester
+            // 
+            this.lblRequester.AutoSize = true;
+            this.lblRequester.Location = new System.Drawing.Point(3, 23);
+            this.lblRequester.Name = "lblRequester";
+            this.lblRequester.Size = new System.Drawing.Size(56, 13);
+            this.lblRequester.TabIndex = 3;
+            this.lblRequester.Text = "Requester";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(3, 50);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(55, 13);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Song Title";
+            // 
+            // tabYouTube
+            // 
+            this.tabYouTube.Location = new System.Drawing.Point(4, 22);
+            this.tabYouTube.Name = "tabYouTube";
+            this.tabYouTube.Padding = new System.Windows.Forms.Padding(3);
+            this.tabYouTube.Size = new System.Drawing.Size(809, 472);
+            this.tabYouTube.TabIndex = 1;
+            this.tabYouTube.Text = "YouTube";
+            this.tabYouTube.UseVisualStyleBackColor = true;
+            // 
+            // SongStarter
+            // 
+            this.SongStarter.Enabled = true;
+            this.SongStarter.Interval = 200;
+            this.SongStarter.Tick += new System.EventHandler(this.SongStarter_Tick);
+            // 
+            // frmKruBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 498);
+            this.Controls.Add(this.TabControl);
+            this.Name = "frmKruBot";
+            this.Text = "KruBot";
+            this.Load += new System.EventHandler(this.frmKruBot_Load);
+            this.TabControl.ResumeLayout(false);
+            this.tabChat.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.SidePanel.ResumeLayout(false);
+            this.SidePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMusicVolume)).EndInit();
+            this.tblInfo.ResumeLayout(false);
+            this.tblInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void TbMsg_TextChanged(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tabChat;
+        private System.Windows.Forms.TabPage tabYouTube;
+        private System.Windows.Forms.Timer SongStarter;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.TableLayoutPanel SidePanel;
+        private System.Windows.Forms.TrackBar tbMusicVolume;
+        private System.Windows.Forms.TableLayoutPanel tblInfo;
+        private System.Windows.Forms.Label lblPlayerTime;
+        private System.Windows.Forms.Label lblRequester;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ KruBot/frmKruBot.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using TwitchLib.Client; //MIT
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using Vlc.DotNet.Core.Interops.Signatures;
+using Vlc.DotNet.Forms; //MIT
+using YoutubeExplode; //GPL3
+using YoutubeExplode.Models.MediaStreams;
+
+
+//TODO:
+// Capture More Information from Song Requests for Display ✔
+// Deny Duplicate Song Requests ✔
+// Display Extra Information ✔
+// 
+namespace KruBot
+{
+    public partial class frmKruBot : Form
+    {
+        public static VlcControl vlc = new VlcControl();
+        public static TwitchClient client = new TwitchClient();
+        public static Queue<songreq> qt = new Queue<songreq>();
+
+        public frmKruBot()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKruBot_Load(object sender, EventArgs e)
+        {
+            vlc.BeginInit();
+            vlc.VlcLibDirectory = Vlc_VlcLibDirectoryNeeded();
+            var options = new[]
+            {
+                "--audio-filter", "normvol", "--norm-max-level", "1.5"
+            };
+            vlc.VlcMediaplayerOptions = options;
+            vlc.EndInit();
+            vlc.Dock = DockStyle.Fill;
+            tabYouTube.Controls.Add(vlc);
+            vlc.Audio.Volume = tbMusicVolume.Value;
+            //VLC set up.
+
+            var cred = JsonConvert.DeserializeObject<creds>(File.ReadAllText("creds.json"));
+            var credentials = new ConnectionCredentials(cred.username, cred.oauth);
+            client.Initialize(credentials, "pfckrutonium");
+            client.OnConnected += Client_OnConnected;
+            client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.Connect();
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            //Build Tags
+            var userTags = "";
+            if (e.ChatMessage.IsMe)
+                userTags += "[Me]";
+            else if (e.ChatMessage.IsModerator)
+                userTags += "[Mod]";
+            else if (e.ChatMessage.IsSubscriber)
+                userTags += "[Sub]";
+            else if (e.ChatMessage.IsTurbo)
+                userTags += "[TwPr]";
+            else if (e.ChatMessage.IsBroadcaster) userTags += "[Me]";
+
+            userTags += " " + e.ChatMessage.Username + ": ";
+            if (rtbChat.InvokeRequired)
+                rtbChat.Invoke((Action) delegate
+                {
+                    rtbChat.AppendText(userTags + e.ChatMessage.Message + Environment.NewLine);
+                });
+            else
+                rtbChat.AppendText(userTags + e.ChatMessage.Message + Environment.NewLine);
+
+            //Act on Commands
+            if (e.ChatMessage.Message.ToLower().StartsWith("!songrequest"))
+                try
+                {
+                    var ytLink = e.ChatMessage.Message.Split(' ');
+                    var url = ytLink[1];
+                    var exists = qt.Any(x => x.ytlink.ToLower() == url.ToLower());
+                    if (exists)
+                    {
+                        client.SendMessage(e.ChatMessage.Channel, "Song already exists in Queue.");
+                    }
+                    else
+                    {
+                        var p = new songreq();
+                        p.requester = e.ChatMessage.Username;
+                        p.ytlink = ytLink[1];
+                        qt.Enqueue(p);
+                        client.SendMessage(e.ChatMessage.Channel, "Added " + GetVideoTitle(ytLink[1]) + " to Queue.");
+                    }
+                }
+                catch
+                {
+                }
+        }
+
+        private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            rtbChat.Invoke((Action) delegate
+            {
+                rtbChat.AppendText("Successfully connected to the channel." + Environment.NewLine);
+            });
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            rtbChat.Invoke((Action) delegate { rtbChat.AppendText("Connected to Twitch." + Environment.NewLine); });
+        }
+
+        private DirectoryInfo Vlc_VlcLibDirectoryNeeded()
+        {
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            // Default installation path of VideoLAN.LibVLC.Windows
+            var VlcLibDirectory =
+                new DirectoryInfo(Path.Combine(currentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+            return VlcLibDirectory;
+        }
+
+        private void SetVideo(songreq request)
+        {
+            try
+            {
+                vlc.Position = 0;
+                var id = YoutubeClient.ParseVideoId(request.ytlink);
+                lblRequester.Text = request.requester;
+                var yt = new YoutubeClient();
+                var video = yt.GetVideoMediaStreamInfosAsync(id).Result;
+                var muxed = video.Muxed.WithHighestVideoQuality();
+                lblTitle.Text = GetVideoTitle(request.ytlink);
+                vlc.SetMedia(new Uri(muxed.Url));
+                vlc.Play();
+            }
+            catch
+            {
+            }
+        }
+
+        private string GetVideoTitle(string url)
+        {
+            try
+            {
+                var id = YoutubeClient.ParseVideoId(url);
+                var yt = new YoutubeClient();
+                var video = yt.GetVideoAsync(id).Result;
+                return video.Title;
+            }
+            catch
+            {
+            }
+
+            return "Not a valid video.";
+        }
+
+        private void SongStarter_Tick(object sender, EventArgs e)
+
+        {
+            var b = (int) vlc.Time / 1000;
+            var a = (int) vlc.Length / 1000;
+            var c = a / 60; //Total Seconds
+            a = a - c * 60; //Total Minutes
+            var d = b / 60; //seconds
+            b = b - d * 60; //Minutes
+            lblPlayerTime.Text = d + @":" + b + @"/" + c + @":" + a;
+            //lblPlayerTime.Text = vlc.Time.ToString();
+            switch (vlc.State)
+            {
+                case MediaStates.NothingSpecial:
+                {
+                    if (qt.Count > 0) SetVideo(qt.Dequeue());
+                    break;
+                }
+                case MediaStates.Ended:
+                {
+                    if (qt.Count > 0) SetVideo(qt.Dequeue());
+                    break;
+                }
+            }
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            client.SendMessage(client.GetJoinedChannel("pfckrutonium"), tbMsg.Text);
+            if (tbMsg.Text.ToLower().StartsWith("!songrequest"))
+                try
+                {
+                    var ytLink = tbMsg.Text.Split(' ');
+                    var p = new songreq
+                    {
+                        requester = "Me",
+                        ytlink = ytLink[1]
+                    };
+                    qt.Enqueue(p);
+                    client.SendMessage(client.GetJoinedChannel("pfckrutonium"),
+                        "Added " + GetVideoTitle(ytLink[1]) + " to Queue.");
+                }
+                catch
+                {
+                }
+
+            rtbChat.Invoke((Action) delegate { rtbChat.AppendText("[Me] " + tbMsg.Text + Environment.NewLine); });
+            tbMsg.Text = "";
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            vlc.Position = 1;
+        }
+
+        private void tbMsg_TextChanged(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            if (e.KeyCode == Keys.Enter) btnSendMessage_Click(this, new EventArgs());
+        }
+
+        private void tbMsg_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private void rtbChat_TextChanged(object sender, EventArgs e)
+        {
+            rtbChat.SelectionStart = rtbChat.Text.Length;
+            rtbChat.ScrollToCaret();
+
+            //Keep our chat scrolling
+        }
+
+        private void tbMusicVolume_Scroll(object sender, EventArgs e)
+        {
+            vlc.Audio.Volume = tbMusicVolume.Value;
+        }
+
+        public class creds
+        {
+            public string oauth;
+            public string username;
+        }
+
+        public class songreq
+        {
+            public string requester;
+            public string ytlink;
+        }
+    }
+}
